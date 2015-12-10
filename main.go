7494eddcd08931b0798da8371148c5fa014f2e74
zhengsai <gavinzhs@gmail.com>
2015-12-10 16:50:33 +0800

    start beegoframeworkdiy

--- main.go
+++ main.go
@@ -1,16 +1,25 @@
 package main
 
 import (
-	"log"
-	"net/http"
-)
+    "beegoDIY"
+    "log"
+    "os"
+    "net/http")
+
 
 func main() {
-	print("start")
+    //    file, err := os.OpenFile(fmt.Sprintf("/Users/zhengsai/Documents/go/src/goFrameworkDIY/log/%s.txt", time.Now().Format("2006-01-02")), os.O_CREATE|os.O_APPEND|os.O_RDWR, os.ModePerm)
+    //    if err != nil{
+    //        beegodiy.Critical("open file err : ", err)
+    //        return
+    //    }
 
-	http.Handle("/", http.FileServer(http.Dir("/Users/zhengsai/Downloads/")))
-	http.Handle("/curl/", http.StripPrefix("/curl/", http.FileServer(http.Dir("/Users/zhengsai/Downloads/curl"))))
-	http.Handle("/make/", http.StripPrefix("/make/", http.FileServer(http.Dir("/Users/zhengsai/Downloads/make"))))
+    //    beegodiy.SetLogger(log.New(file, "[文件]", log.Ldate|log.Ltime))
+    beegodiy.SetLogger(log.New(os.Stdout, "[beegodiy]", log.Ldate|log.Ltime))
+    beegodiy.Trace("start\n")
+    //    	http.Handle("/log/", http.StripPrefix("/log/", http.FileServer(http.Dir("/Users/zhengsai/Documents/go/src/goFrameworkDIY/log/"))))
 
-	log.Fatalln(http.ListenAndServe(":9000", nil))
+    //	http.Handle("/curl/", http.StripPrefix("/curl/", http.FileServer(http.Dir("/Users/zhengsai/Downloads/curl"))))
+    //	http.Handle("/make/", http.StripPrefix("/make/", http.FileServer(http.Dir("/Users/zhengsai/Downloads/make"))))
+    log.Fatalln(http.ListenAndServe(":9000", nil))
 }
