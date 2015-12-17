ed970deb94709d014772d7d3876f17d1f003a9c8
zhengsai <gavinzhs@gmail.com>
2015-12-17 17:19:56 +0800

    minor

--- main.go
+++ main.go
@@ -1,27 +1,25 @@
 package main
 
 import (
-	"beegoDIY"
-	"fmt"
 	"log"
 	"net/http"
-	"os"
-	"time"
 )
 
 func main() {
-	file, err := os.OpenFile(fmt.Sprintf("/Users/zhengsai/Documents/go/src/goFrameworkDIY/log/%s.txt", time.Now().Format("2006-01-02")), os.O_CREATE|os.O_APPEND|os.O_RDWR, os.ModePerm)
-	if err != nil {
-		beegodiy.Critical("open file err : ", err)
-		return
-	}
-
-	beegodiy.SetLogger(log.New(file, "[文件]", log.Ldate|log.Ltime))
-	//	beegodiy.SetLogger(log.New(os.Stdout, "[beegodiy]", log.Ldate|log.Ltime))
-	beegodiy.Trace("start")
-		http.Handle("/log/", http.StripPrefix("/log/", http.FileServer(http.Dir("/Users/zhengsai/Documents/go/src/goFrameworkDIY/log/"))))
-//	http.Handle("/log/", http.FileServer(http.Dir("/Users/zhengsai/Documents/go/src/goFrameworkDIY/log/")))
-
+	//	file, err := os.OpenFile(fmt.Sprintf("/Users/zhengsai/Documents/go/src/goFrameworkDIY/log/%s.txt", time.Now().Format("2006-01-02")), os.O_CREATE|os.O_APPEND|os.O_RDWR, os.ModePerm)
+	//	if err != nil {
+	//		beegodiy.Critical("open file err : ", err)
+	//		return
+	//	}
+	//
+	//	beegodiy.SetLogger(log.New(file, "[文件]", log.Ldate|log.Ltime))
+	//	//	beegodiy.SetLogger(log.New(os.Stdout, "[beegodiy]", log.Ldate|log.Ltime))
+	//	beegodiy.Trace("start")
+	//		http.Handle("/log/", http.StripPrefix("/log/", http.FileServer(http.Dir("/Users/zhengsai/Documents/go/src/goFrameworkDIY/log/"))))
+	//	http.Handle("/log/", http.FileServer(http.Dir("/Users/zhengsai/Documents/go/src/goFrameworkDIY/log/")))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello"))
+	})
 	//	rg := beegodiy.NewControllerRegistor()
 	//	rg.Add("/post/list", &PostList{})
 	//	rg.Add("/post/:id", &Post{})
